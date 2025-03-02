9e3af9170364a602501941d8a3bf05727f2bf0ee
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-03-02 11:56:40 +0530

    pages added tba

---
+++ app/certifications/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>certifications page</div>
+  )
+}
+
+export default page
---
+++ app/gaming/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>gaming page</div>
+  )
+}
+
+export default page
